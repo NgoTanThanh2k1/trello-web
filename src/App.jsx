a86a578804c14829b2254c96564fedfd70425228
ThanhNgo2k1 <ngotanthanh1342001@gmail.com>
2025-06-30 15:24:00 +0700

    new
    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-
-
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import { Box, Typography } from '@mui/material'
-import {useColorScheme} from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-
- function ModeSelect() {
-
-  const { mode, setMode } = useColorScheme()
+import Button from "@mui/material/Button";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
+import HomeIcon from "@mui/icons-material/Home";
+import { pink } from "@mui/material/colors";
+import { Box, Container, Typography } from "@mui/material";
+import { useColorScheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import theme from "./theme";
+// import Container from "@mui/material/Container";
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
   const handleChange = (event) => {
-    const selectedMode = (event.target.value)
-    setMode(selectedMode)
-    console.log(selectedMode)
+    const selectedMode = event.target.value;
+    setMode(selectedMode);
+    console.log(selectedMode);
   };
 
   return (
@@ -36,69 +34,64 @@ import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
         onChange={handleChange}
       >
         <MenuItem value="light">
-          <div style={{display: 'flex', alignItems: 'center', gap: '8px'}}>
-            <LightModeIcon fontSize="small"/> Light
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <LightModeIcon fontSize="small" /> Light
           </div>
         </MenuItem>
         <MenuItem value="dark">
-        <Box sx={{display: 'flex', alignItems: 'center', gap: '8px'}}>
-            <DarkModeOutlinedIcon fontSize="small"/> Dark
+          <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <DarkModeOutlinedIcon fontSize="small" /> Dark
           </Box>
         </MenuItem>
         <MenuItem value="system">
-          <Box sx={{display: 'flex', alignItems: 'center', gap: '8px'}}>
-            <SettingsBrightnessIcon fontSize="small"/> System
+          <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <SettingsBrightnessIcon fontSize="small" /> System
           </Box>
-          </MenuItem>
+        </MenuItem>
       </Select>
     </FormControl>
   );
 }
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)');
-  // console.log('prefersDarkMode: ', prefersDarkMode)
-  // console.log('prefersLightMode: ', prefersLightMode)
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
 
 function App() {
-
-
   return (
-    <>
-    <ModeSelect />
-    <hr/>
-      <ModeToggle></ModeToggle>
-      <hr></hr>
-      <div>Thanhngodev</div>
-      <Typography variant='body2' color="text.secondary">Test Typo</Typography>
-      <Button variant="contained">Hello world</Button>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br/>
-      <AccessAlarmIcon></AccessAlarmIcon>
-      <ThreeDRotation></ThreeDRotation>
-
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
-    </>
-  )
+    <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
+      <Box
+        sx={{
+          backgroundColor: "primary.light",
+          width: "100%",
+          height: (theme) => theme.trello.appBarHeight,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <ModeSelect />
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.dark",
+          width: "100%",
+          height: (theme) => theme.trello.boardBarHeight,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        Board Bar
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+          width: "100%",
+          height: (theme) =>
+            `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        Board Content
+      </Box>
+    </Container>
+  );
 }
 
-export default App
+export default App;
