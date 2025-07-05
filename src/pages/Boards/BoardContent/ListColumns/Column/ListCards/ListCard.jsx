58241ad60092c378814d79ad5b648ef4c79dd944
ThanhNgo2k1 <ngotanthanh1342001@gmail.com>
2025-07-05 20:19:52 +0700

    dragover
    dragover

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCard.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCard.jsx
@@ -1,35 +1,43 @@
 import React from "react";
 import { Box, Container, Typography } from "@mui/material";
 import Card from "./Card/Card";
-
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 function ListCard({ cards }) {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 1,
-        p: "0 5px",
-        m: "0 5px",
-        overflowx: "hidden",
-        overflowY: "auto",
-        maxHeight: (theme) =>
-          `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)} - ${
-            theme.trello.columnHeaderHeight
-          } - ${theme.trello.columnFooterHeight})`,
-        "&::-webkit-scrollbar-thumb": {
-          backgroundColor: "#ced0da",
-          borderRadius: "8px",
-        },
-        "&::-webkit-scrollbar-thumb:hover": {
-          backgroundColor: "#bfc2cf",
-        },
-      }}
+    <SortableContext
+      items={cards?.map((c) => c._id)}
+      strategy={verticalListSortingStrategy}
     >
-      {cards?.map((card) => (
-        <Card key={card._id} card={card} />
-      ))}
-    </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 1,
+          p: "0 5px",
+          m: "0 5px",
+          overflowx: "hidden",
+          overflowY: "auto",
+          maxHeight: (theme) =>
+            `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)} - ${
+              theme.trello.columnHeaderHeight
+            } - ${theme.trello.columnFooterHeight})`,
+          "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "#ced0da",
+            borderRadius: "8px",
+          },
+          "&::-webkit-scrollbar-thumb:hover": {
+            backgroundColor: "#bfc2cf",
+          },
+        }}
+      >
+        {cards?.map((card) => (
+          <Card key={card._id} card={card} />
+        ))}
+      </Box>
+    </SortableContext>
   );
 }
 
