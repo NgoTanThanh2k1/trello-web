e1f575014b62eb67e4778c6a60a6dda72143b23d
ThanhNgo2k1 <ngotanthanh1342001@gmail.com>
2025-07-06 17:29:44 +0700

    fixbugcolumn
    fixbugcolumn

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import theme from "~/theme";
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 import { ThemeProvider } from "@emotion/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CssVarsProvider theme={theme}>
-      <App />
-      <CssBaseline />
-    </CssVarsProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <CssVarsProvider theme={theme}>
+    <App />
+    <CssBaseline />
+  </CssVarsProvider>
+  // </React.StrictMode>
 );
